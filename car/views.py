22f4e09ed45dc67db55bbea9ccbf6c6f27de26a8
Mehmet Arslan <marslan.developer@gmail.com>
2023-01-30 19:53:14 +0300

    done

--- car/views.py
+++ car/views.py
@@ -8,6 +8,9 @@ from .permissions import IsStaffOrReadOnly
 
 from django.db.models import Q
 
+from django.utils import timezone
+from rest_framework.response import Response
+
 
 class CarView(ModelViewSet):
     queryset = Car.objects.all()
@@ -52,3 +55,28 @@ class ReservationView(ListCreateAPIView):
         if self.request.user.is_staff:
             return super().get_queryset()
         return super().get_queryset().filter(customer=self.request.user)
+
+
+class ReservationDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
+    # lookup_field = 'id'
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(
+            instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        end = serializer.validated_data.get('end_date')
+        car = serializer.validated_data.get('car')
+        start = instance.start_date
+        today = timezone.now().date()
+        if Reservation.objects.filter(car=car).exists():
+            # a = Reservation.objects.filter(car=car, start_date__gte=today)
+            # print(len(a))
+            for res in Reservation.objects.filter(car=car, end_date__gte=today):
+                if start < res.start_date < end:
+                    return Response({'message': 'Car is not available...'})
+
+        return super().update(request, *args, **kwargs)
